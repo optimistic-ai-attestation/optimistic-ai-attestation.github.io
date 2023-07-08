dfa5e5e122d9ffb3edcbfcd0bd5b58efd38f8ab9
jordan <jordan@bitola.com>
2023-07-07 21:26:35 -0400

    staic

---
+++ _next/static/NwGxPUsHLhEwC7QVJZStC/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/167-43c051c71e309141.js","static/chunks/pages/index-be0799027e5f7d82.js"],"/_error":["static/chunks/pages/_error-13b031acb53927db.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
